b05fc5ea0bb47e853d268c6f54bcd6df3137d440
Tristan Rubadeau <tristan@rubadeau.com>
2015-02-27 15:33:09 -0700

    belongs_to and has_many

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,3 +1,12 @@
 class CompaniesController < ApplicationController
 
+  def index
+    @companies = Company.all
+    @company = Company.new
+    @event = Event.new
+  end
+
+  def show
+    @company = Company.find(params[:id])
+  end
 end
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,30 @@
 class EventsController < ApplicationController
+  before_action :find_and_set_company
 
+  def index
+    @events = Company.events
+  end
 
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = @company.events.new(event_params)
+    if @event.save
+      redircts_to company_path(params(:company_id))
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event), permit(:name)
+  end
+
+  def find_and_set_company
+    @company = Company.find(params[:company_id])
+  end
 end
